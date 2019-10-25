386dbb1d6b152c274ce205e1f9c6950e59313b18
Trevor <tismetrm@gmail.com>
2019-10-25 14:17:06 -0700

    Complete logic in order to diplay proper language to learn. Fix indentation. Reset counters at the culmination of the program. Add logic to display on one paragraph at a time so the program can be reusable without refreshing.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,5 @@
 $(document).ready(function () {
 
-  let jsCounter = 0;
-  let rubyCounter = 0;
-  let cCounter = 0;
-
   $('#survey-form').submit(function(event) {
     event.preventDefault();
 
@@ -13,7 +9,9 @@ $(document).ready(function () {
     var fourthAnswer = parseInt($("#question-4").val());
     var fifthAnswer = parseInt($("#question-5").val());
 
-
+    let jsCounter = 0;
+    let rubyCounter = 0;
+    let cCounter = 0;
 
 // Question One
 
@@ -75,25 +73,48 @@ $(document).ready(function () {
       cCounter += 1;
     }
 
-// Final text to inserted in spans within paragraphs.
+// Based on counters, call elements with jQuery, insert text, and display paragraph.
 
     if (jsCounter >= 3){
-      $("#single-result").text("Javascript");
+      $(".tie-result-para").hide();
+      $("#single-result").empty().append("Javascript");
       $(".result-para").show();
     }
 
+    if (jsCounter === 2){
+      if (rubyCounter === 2){
+        $("#tie-second-result").empty().append("Ruby");
+      }
+      else {
+        $("#tie-second-result").text("C#")
+      }
+      $(".result-para").hide();
+      $("#tie-first-result").empty().append("Javascript");
+      $(".tie-result-para").show();
+    }
 
+    if (rubyCounter >= 3){
+      $(".tie-result-para").hide();
+      $("#single-result").empty().append("Ruby");
+      $(".result-para").show();
+    }
 
+    if (rubyCounter === 2 && cCounter === 2){
+      $(".result-para").hide();
+      $("#tie-first-result").empty().append("Ruby");
+      $("#tie-second-result").empty().append("C#");
+      $(".tie-result-para").show();
+    }
 
-
+    if (cCounter >= 3){
+      $(".tie-result-para").hide();
+      $("#single-result").empty().append("C#");
+      $(".result-para").show();
+    }
 
     jsCounter = 0;
     rubyCounter = 0;
     cCounter = 0;
 
-
-
-
-  });         // This is the form submit closing bracket.
-
+  });
 });
