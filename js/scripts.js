e7939e711c61f9cc3ef1a6b30b8aed24e3b4a14a
Trevor <tismetrm@gmail.com>
2019-10-25 11:07:16 -0700

    Add variables that use jQuery to target input field values. Update each form group with values (numbers that will parsed in variables to make conditonal statements flow better. Add counters that increase based on what selections the user will make.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,27 @@
 $(document).ready(function () {
   $('#survey-form').submit(function(event) {
     event.preventDefault();
+
+    var firstAnswer = parseInt($("#question-1").val());
+    var secondAnswer = parseInt($("#question-2").val());
+    var thirdAnswer = parseInt($("#question-3").val());
+    var fourthAnswer = parseInt($("#question-4").val());
+    var fifthAnswer = parseInt($("#question-5").val());
+
+    let jsCounter = 0;
+    let rubyCounter = 0;
+    let cCounter = 0;
+
+    if (firstAnswer === 1){
+      jsCounter += 1;
+    }
+    if (jsCounter >= 1) {
+        alert(jsCounter);
+    }
+
+
+
+
   })
 
 
